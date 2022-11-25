b132b10042c0ef79015bdb58c38563a697b3b3b5
Diego Barros <diegobarros@users.noreply.github.com>
2022-11-24 22:43:18 -0300

    Gabarito Aula JDBC

--- src/main/java/org/exemplo/Main.java
+++ src/main/java/org/exemplo/Main.java
@@ -2,9 +2,7 @@ package org.exemplo;
 
 import org.exemplo.models.Produto;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Properties;
@@ -13,8 +11,8 @@ public class Main {
 
     static final String DRIVER_BD = "com.mysql.cj.jdbc.Driver";
     static final String URL_BD = "jdbc:mysql://localhost:3306/estoque";
-    static final String USUARIO_BD = "";
-    static final String SENHA_BD = "";
+    static final String USUARIO_BD = "root";
+    static final String SENHA_BD = "12345";
 
     /**
      * Registra o driver MySql para conexão com o SGBD.
@@ -59,35 +57,174 @@ public class Main {
         return conexao;
     }
 
-    static int inserirProduto(Produto produto) {
-        // TODO Implemente a inserção de um produto aqui
-        // Retorna o número do cliente recém-criado ou -1 em caso de erro
-        return -1;
+    /**
+     * Adiciona um novo registro no banco de dados
+     * @param produto o novo produto que será adicionado
+     * @return o id do cliente recém-criado ou -1 em caso de erro
+     */
+    static long inserirProduto(Produto produto) {
+
+        long idProduto = -1L;
+        String query = "INSERT INTO Produto(nome, preco, quantidade) VALUES (?, ?, ?);";
+
+        try (Connection conexao = criarConexao()) {
+
+            PreparedStatement comando = conexao.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            comando.setString(1, produto.getNome());
+            comando.setDouble(2, produto.getPreco());
+            comando.setInt(3, produto.getQuantidade());
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            if (linhasAfetadas == 1) {
+
+                ResultSet resultado = comando.getGeneratedKeys();
+
+                while (resultado.next())
+                    idProduto = resultado.getLong(1);
+
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return idProduto;
     }
 
+    /**
+     * Apaga um registro no banco de dados
+     * @param id chave primária do produto
+     * @return true em caso de sucesso ou false em caso de falha
+     */
     static boolean excluirProduto(long id) {
-        // TODO Implemente a exclusão de um produto aqui
-        // Retorna true em caso de sucesso ou false em caso de falha
-        return false;
+
+        boolean excluido = false;
+        String query = "DELETE FROM Produto WHERE id = ?;";
+
+        try (Connection conexao = criarConexao()) {
+
+            PreparedStatement comando = conexao.prepareStatement(query);
+            comando.setLong(1, id);
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            if (linhasAfetadas == 1)
+                excluido = true;
+
+        } catch (SQLException e) {
+
+            System.err.println(e.getMessage());
+
+        }
+
+        return excluido;
     }
 
+    /**
+     * Seleciona um produto do banco de dados através do seu ID
+     * @param id chave primária do produto
+     * @return um objeto do Produto encontrado, ou null se não encontrado ou em caso de erro
+     */
     static Produto buscarProduto(long id) {
-        // TODO Implemente a busca de um produto aqui
-        // usando valores de argumento fornecidos como critérios de pesquisa
-        // Retorna um objeto do Produto encontrado, ou null se não encontrado ou em caso de erro
-        return null;
+
+        Produto produto = null;
+
+        String query = "SELECT * FROM Produto WHERE id = ?;";
+
+        try (Connection conexao = criarConexao()) {
+
+            PreparedStatement comando = conexao.prepareStatement(query);
+            comando.setLong(1, id);
+
+            ResultSet resultado = comando.executeQuery();
+
+            while (resultado.next()) {
+
+                long idProduto = resultado.getLong("id");
+                String nomeProduto = resultado.getString("nome");
+                double preco = resultado.getDouble("preco");
+                int quantidade = resultado.getInt("quantidade");
+
+                produto = new Produto(idProduto, nomeProduto, preco, quantidade);
+            }
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return produto;
     }
 
+    /**
+     * Obtém todos os produtos
+     * @return um objeto do Produto encontrado, ou null se não encontrado ou em caso de erro
+     */
     static ArrayList<Produto> buscarTodosProdutos() {
-        // TODO Implemente a busca de todos os produtos aqui
-        // Retorna um objeto do Produto encontrado, ou null se não encontrado ou em caso de erro
-        return null;
+
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+        String query = "SELECT * FROM Produto;";
+
+        try (Connection conexao = criarConexao()) {
+
+            Statement comando = conexao.createStatement();
+            ResultSet resultado = comando.executeQuery(query);
+
+            while (resultado.next()) {
+
+                Produto produto = new Produto();
+
+                long id = resultado.getLong("id");
+                String nomeProduto = resultado.getString("nome");
+                double preco = resultado.getDouble("preco");
+                int quantidade = resultado.getInt("quantidade");
+
+                produtos.add(new Produto(id, nomeProduto, preco, quantidade));
+
+            }
+
+
+        } catch (SQLException e) {
+
+            System.err.println("SQLException: " + e.getMessage());
+
+        }
+
+        return produtos;
     }
 
+    /**
+     * Atualiza um produto no banco de dados
+     * @param produto produto com dados atualizados
+     * @return true em caso de sucesso ou false em caso de falha ou erro
+     */
     static boolean atualizarProduto(Produto produto) {
-        // TODO Implemente a atualização de um registro aqui usando dados do objeto Produto
-        // Retorna true em caso de sucesso ou false em caso de falha ou erro
-        return false;
+
+        boolean produtoAtualizado = false;
+
+        String query = "UPDATE Produto SET nome = ?, preco = ?, quantidade = ? WHERE id = ?;";
+
+        try (Connection conexao = criarConexao()) {
+
+            PreparedStatement comando = conexao.prepareStatement(query);
+
+            comando.setString(1, produto.getNome());
+            comando.setDouble(2, produto.getPreco());
+            comando.setInt(3, produto.getQuantidade());
+            comando.setLong(4, produto.getId());
+
+            int linhasAfetadas = comando.executeUpdate();
+
+            if (linhasAfetadas == 1)
+                produtoAtualizado = true;
+
+        } catch (SQLException e) {
+
+            System.err.println(e.getMessage());
+
+        }
+
+        return produtoAtualizado;
     }
 
     /**
@@ -136,15 +273,15 @@ public class Main {
         Produto monitor = new Produto(3, "Monitor Dell de 27\" 4K com Hub USB-C P2723QE", 2899.00, 500);
         Produto bicicleta = new Produto(4, "Bicicleta Aro 29 Shimano MTB Pontal Blitz Alumínio - Cinza", 999.00, 1);
 
-        ArrayList<Produto> produtos = new ArrayList<Produto>();
-        produtos.add(iphone);
-        produtos.add(macbook);
-        produtos.add(monitor);
-        produtos.add(bicicleta);
+        // ArrayList<Produto> produtos = new ArrayList<Produto>();
+        // produtos.add(iphone);
+        // produtos.add(macbook);
+        // produtos.add(monitor);
+        // produtos.add(bicicleta);
 
-        mostrarProduto(bicicleta);
-        mostrarTabelaDeProdutos(produtos);
-        mostrarProduto(bicicleta);
+        // mostrarProduto(bicicleta);
+        // mostrarTabelaDeProdutos(produtos);
+        // mostrarProduto(bicicleta);
 
         /**********************************************************************************
          *                               TESTANDO O CRUD                                  *
@@ -153,26 +290,31 @@ public class Main {
          **********************************************************************************/
 
         // CREATE: vamos cadastrar, adicionar, inserir ou salvar um registro no banco de dados.
-        // inserirProduto(iphone);
-        // inserirProduto(macbook);
-        // inserirProduto(monitor);
-        // inserirProduto(bicicleta);
+        // long idIphone = inserirProduto(iphone);
+        // long idMacbook = inserirProduto(macbook);
+        // long idMonitor = inserirProduto(monitor);
+        // long idBicicleta = inserirProduto(bicicleta);
 
 
         // READ: vamos Ler, recuperar ou selecionar os dados no banco de dados.
-        // ArrayList<Produto> produtos = buscarTodosProdutos();
+        ArrayList<Produto> produtos = buscarTodosProdutos();
+        mostrarTabelaDeProdutos(produtos);
         // Produto p = buscarProduto(1);
+        // mostrarProduto(p);
 
 
         // UPDATE: vamos atualizar, modificar ou alterar um registro no banco de dados.
-        // Produto monitorDesatualizado = buscarProduto(3);
-        // monitorDesatualizado.setQuantidade(300);
-        // monitorDesatualizado.setPreco(2612.72);
-        // atualizarProduto(monitorDesatualizado);
-
+        Produto monitorDesatualizado = buscarProduto(10);
+        mostrarProduto(monitorDesatualizado);
+        monitorDesatualizado.setQuantidade(300);
+        monitorDesatualizado.setPreco(2612.72);
+        atualizarProduto(monitorDesatualizado);
+        mostrarProduto(monitorDesatualizado);
 
         // DELETE: vamos excluir, remover ou apagar um registro no banco de dados.
-        // excluirProduto(4);
+        // boolean excluido = excluirProduto(8);
+        // System.out.println("Produto Exlcuído? " + excluido);
+        // mostrarTabelaDeProdutos(produtos);
         
 
     } // Fim do método main
